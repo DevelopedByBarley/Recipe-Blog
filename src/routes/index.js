190a357ab1e28ac423f9b6a58d94012922d78ec9
Árpád <developedbybarley@gmail.com>
2022-08-12 11:42:53 +0200

    Hungarian, Italian, Chinese and American Latests

--- src/routes/index.js
+++ src/routes/index.js
@@ -7,9 +7,23 @@ router.get('/', async (req, res) => {
   await new RecipeModel()
   await new CategoriesModel();
   let latestRecipes = await RecipeModel.find({}).sort({ _id: -1 }).limit(5);
-  let latestCategories = await CategoriesModel.find({}).sort({_id : -1}).limit(5);
+  let latestCategories = await CategoriesModel.find({}).sort({ _id: -1 }).limit(5);
+  let americanLatest = await RecipeModel.find({ categorie: '62f259b3849a591f6a0e1474' }).sort({ _id: -1 }).limit(5);
+  let chineseLatest = await RecipeModel.find({ categorie: '62f259dddef23d8a732190e1' }).sort({ _id: -1 }).limit(5);
+  let hungarianLatest = await RecipeModel.find({ categorie: '62f259a6849a591f6a0e146d' }).sort({ _id: -1 }).limit(5);
+  let italianLatest = await RecipeModel.find({ categorie: '62f259f5def23d8a732190ef' }).sort({ _id: -1 }).limit(5);
+  console.log(chineseLatest)
 
-  res.render('index', { latestRecipes: latestRecipes, latestCategories: latestCategories })
+  res.render('index',
+    {
+      latestRecipes: latestRecipes,
+      latestCategories: latestCategories,
+      americanLatest: americanLatest,
+      chineseLatest: chineseLatest,
+      hungarianLatest: hungarianLatest,
+      italianLatest: italianLatest
+    }
+  )
 })
 
 module.exports = router;
