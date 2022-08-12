4764ceccf9c4de7167a2adc8ff5b96bc32ba3602
Árpád <developedbybarley@gmail.com>
2022-08-12 10:50:33 +0200

    delete confirm box

--- src/public/js/recipes/allRecipes.js
+++ src/public/js/recipes/allRecipes.js
@@ -1,20 +1,38 @@
 let deleteRecipeBtn = document.querySelectorAll('.deleteRecipeBtn');
+let confirm = document.getElementById('confirm');
+let accept = document.getElementById('accept');
+let reject = document.getElementById('reject')
+
 
 for (let deleteBtn of deleteRecipeBtn) {
   deleteBtn.addEventListener('click', async (event) => {
     let id = event.currentTarget.dataset.id;
-    console.log(id);
-    try {
 
-      await fetch(`/recipes/recipe/${id}`, {
-        method: 'DELETE',
-      })
-      window.location.assign('/recipes')
+    confirm.classList.add('active')
+
+    accept.onclick = async () => {
+      try {
+
+        await fetch(`/recipes/recipe/${id}`, {
+          method: 'DELETE',
+        })
+        window.location.assign('/recipes')
 
 
-    } catch (error) {
-      window.location.assign('/')
-      console.log(error)
+      } catch (error) {
+        window.location.assign('/')
+        console.log(error)
+      }
     }
+
+    reject.onclick = () => {
+      confirm.classList.remove('active')
+    }
+
+
+
+
   })
+
 }
+
