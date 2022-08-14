2fd613d8f04eb7e4d9cc79df1dd3908efd92f02f
Árpád <developedbybarley@gmail.com>
2022-08-14 13:24:44 +0200

    ingredients menu toggle

--- src/public/js/recipes/recipe.js
+++ src/public/js/recipes/recipe.js
@@ -1,10 +1,29 @@
 const ingredients = document.getElementById('ingredients')
+const ingredientsMenu = document.getElementById('ingredients-menu')
+const ingredientsHide = document.getElementById('hide-menu')
 let bounding = ingredients.getBoundingClientRect();
 let bottom = bounding.bottom;
 window.addEventListener('scroll', (event) => {
   let scrollY = window.scrollY;
   console.log(bottom, scrollY)
   if(scrollY > bottom) {
-    console.log('Hello')
-  } 
+    ingredientsMenu.classList.add('active')
+    ingredients.classList.add('fixed')
+  } else {
+    ingredientsMenu.classList.remove('active')
+    ingredients.classList.remove('fixed')
+  }
 })
+
+ingredientsMenu.addEventListener('click', () => {
+  ingredients.classList.add('active');
+  ingredientsMenu.style.display = 'none';
+  ingredientsHide.style.display = 'block';
+})
+
+
+ingredientsHide.onclick = () => {
+  ingredients.classList.remove('active');
+  ingredientsMenu.style.display = 'block';
+  ingredientsHide.style.display = 'none';
+}
